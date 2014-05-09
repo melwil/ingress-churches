061e9eefab040dedafed0d12341a4cf4a6ed13af
melwil <slettvold@gmail.com>
2014-05-09 03:12:02 +0200

    Some more setup.

---
+++ churches/src/main/java/net/melwil/churches/Churches.java
@@ -0,0 +1,11 @@
+package net.melwil.churches;
+
+/**
+ * Hello world!
+ *
+ */
+public class Churches {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
