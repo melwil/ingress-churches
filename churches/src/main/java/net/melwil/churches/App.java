b0de9a98699c1108f4c55ce8a4c0def16979694c
melwil <slettvold@gmail.com>
2014-05-09 03:02:41 +0200

    Initial setup.

---
+++ churches/src/main/java/net/melwil/churches/App.java
@@ -0,0 +1,13 @@
+package net.melwil.churches;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
